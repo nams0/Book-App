9cc968d291ae9b8ac1eeae77596f6aff831ece98
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-06-30 00:47:00 +0330

    added Layout component and it's stlye

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,9 +1,17 @@
+import styles from "./Layout.module.css"
 function Layout({ children }) {
   return (
     <>
-      <header>header</header>
+      <header style={styles.header}>
+        <h1>Book App</h1>
+        <p>
+          <a href="https://github.com/nams0">Nams0</a> | React.js
+        </p>
+      </header>
       {children}
-      <footer>footer</footer>
+      <footer style={styles.footer}>
+        <p>Developed by Amirhossein with ❤️</p>
+      </footer>
     </>
   )
 }
