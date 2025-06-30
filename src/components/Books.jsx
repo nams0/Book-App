46a93d80cc31c3b8ec6d7dbea76058903ad4d58f
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2025-06-30 17:54:13 +0330

    added SideCard component and it's style for favorite books

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,15 +1,42 @@
+import { useState } from "react"
+
 import { books } from "../constants/mockData"
 import BookCard from "./BookCard"
+import SideCard from "./SideCard"
+
+import styles from "./Books.module.css"
 
 function Books() {
+  const [liked, setLiked] = useState([])
+
+  const handelLikedList = (book, status) => {
+    if (status) {
+      const newLikedList = liked.filter((i) => i.id !== book.id)
+      setLiked(newLikedList)
+    } else {
+      setLiked((liked) => [...liked, book])
+    }
+  }
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.cards}>
         {books.map((book) => (
-          <BookCard key={book.id} data={book} />
+          <BookCard
+            key={book.id}
+            data={book}
+            handelLikedList={handelLikedList}
+          />
         ))}
       </div>
-      <div></div>
+      {Boolean(liked.length) && (
+        <div className={styles.favorite}>
+          <h4>Favorites</h4>
+          {liked.map((likedBook) => (
+            <SideCard key={likedBook.id} likedBook={likedBook} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
